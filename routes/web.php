0d582ae0874b4a8302b62204eb7bcc4b67b322e5
岡田敏裕 <web@nuevo.jp>
2020-07-27 00:13:05 +0900

    パラメータを利用する（P37）

--- routes/web.php
+++ routes/web.php
@@ -18,32 +18,32 @@ Route::get('/', function () {
 });
 
 
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {
-    font-size:16px;
-    color:#999;
-}
-h1 {
-    font-size:100px;
-    text-align:right;
-    color:#eee;
-    margin:-40px 0px -50px 0px;
-}
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <p>This is sample page.</p>
-    <p>これは、サンプルで作ったページです。</p>
-</body>
-</html>
-EOF;
 
-Route::get('hello', function () use($html) {
+Route::get('hello/{msg}', function ($msg) {
+    $html = <<<EOF
+    <html>
+    <head>
+    <title>Hello</title>
+    <style>
+    body {
+        font-size:16px;
+        color:#999;
+    }
+    h1 {
+        font-size:100px;
+        text-align:right;
+        color:#eee;
+        margin:-40px 0px -50px 0px;
+    }
+    </style>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        <p>{$msg}</p>
+        <p>これは、サンプルで作ったページです。</p>
+    </body>
+    </html>
+    EOF;
     return $html;
 });
 
